c320971d62a4500edc78349963c0725241fa17a2
Leiverin <vanhungmob@gmail.com>
2022-04-09 20:20:39 +0700

    CORS Enable

--- src/app/controllers/WebAPIController.js
+++ src/app/controllers/WebAPIController.js
@@ -2,8 +2,11 @@ import Item from '../models/item.js';
 import HandleMongoose from '../../util/MongooseHandler.js';
 class WebAPIController{
      GetData(req, res, next){
+          res.header('Access-Control-Allow-Origin', '*');
+          res.header('Access-Control-Allow-Methods', 'GET');
           Item.find({})
           .then(response=>{
+               
                res.send(HandleMongoose.handleWithMultipleObjects(response));
           })
           .catch(next);
